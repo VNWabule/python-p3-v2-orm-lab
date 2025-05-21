60763d7e57950b1a15b60a05541cf00bbd44062e
VNWabule <vicwabule@gmail.com>
2025-05-21 16:01:19 +0300

    done

--- lib/review.py
+++ lib/review.py
@@ -19,6 +19,40 @@ class Review:
             f"<Review {self.id}: {self.year}, {self.summary}, "
             + f"Employee: {self.employee_id}>"
         )
+    
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Year must be an integer.")
+        if value < 2000:
+            raise ValueError("Year must be >= 2000.")
+        self._year = value
+
+    # -------- Summary Property --------
+    @property
+    def summary(self):
+        return self._summary
+
+    @summary.setter
+    def summary(self, value):
+        if not isinstance(value, str) or len(value.strip()) == 0:
+            raise ValueError("Summary must be a non-empty string.")
+        self._summary = value
+
+    
+    @property
+    def employee_id(self):
+        return self._employee_id
+
+    @employee_id.setter
+    def employee_id(self, value):
+        if not isinstance(value, int) or not Employee.find_by_id(value):
+            raise ValueError("Employee ID must reference an existing employee.")
+        self._employee_id = value
 
     @classmethod
     def create_table(cls):
@@ -29,7 +63,8 @@ class Review:
             year INT,
             summary TEXT,
             employee_id INTEGER,
-            FOREIGN KEY (employee_id) REFERENCES employee(id))
+            FOREIGN KEY (employee_id) REFERENCES employee(id)
+            );
         """
         CURSOR.execute(sql)
         CONN.commit()
@@ -47,36 +82,65 @@ class Review:
         """ Insert a new row with the year, summary, and employee id values of the current Review object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
+        sql = """
+        INSERT INTO reviews (year, summary, employee_id)
+        VALUES (?, ?, ?)
+        """
+        CURSOR.execute(sql, (self.year, self.summary, self.employee_id))
+        self.id = CURSOR.lastrowid
+        CONN.commit()
         pass
 
     @classmethod
     def create(cls, year, summary, employee_id):
         """ Initialize a new Review instance and save the object to the database. Return the new instance. """
-        pass
+        review = cls(year, summary, employee_id)
+        review.save()
+        return review
+    
    
     @classmethod
     def instance_from_db(cls, row):
         """Return an Review instance having the attribute values from the table row."""
-        # Check the dictionary for  existing instance using the row's primary key
-        pass
+        if row:
+         return cls(row[1], row[2], row[3], row[0])
    
 
     @classmethod
     def find_by_id(cls, id):
         """Return a Review instance having the attribute values from the table row."""
-        pass
+        CURSOR.execute("SELECT * FROM reviews WHERE id = ?", (id,))
+        row = CURSOR.fetchone()
+        if row:
+            return Review.instance_from_db(row)
+        return None
+        
 
     def update(self):
         """Update the table row corresponding to the current Review instance."""
+        CURSOR.execute(
+        "UPDATE reviews SET year = ?, summary = ?, employee_id = ? WHERE id = ?",
+        (self.year, self.summary, self.employee_id, self.id)
+        )
+
         pass
 
     def delete(self):
         """Delete the table row corresponding to the current Review instance,
         delete the dictionary entry, and reassign id attribute"""
+        CURSOR.execute("DELETE FROM reviews WHERE id = ?", (self.id,))
+        self.id = None
         pass
 
     @classmethod
     def get_all(cls):
         """Return a list containing one Review instance per table row"""
+        sql = "SELECT * FROM reviews"
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        return [cls.instance_from_db(row) for row in rows]
         pass
+    
+    
+     
 
